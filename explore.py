9a0b4ce85cee139aa7643caf3719233a4b220ba7
mendiadi <adim333@gmail.com>
2022-11-28 17:26:56 +0200

    frontend init

--- explore.py
+++ explore.py
@@ -1,30 +1,67 @@
+import enum
+import hashlib
 import threading
 import time
 import tkinter as tk
+from tkinter import ttk
 
+
+
+class AppStates(enum.Enum):
+    LOGIN = "login"
+    EXPLORE = "explore"
+
+
+class ComponentCreator:
+    @staticmethod
+    def create_text_label(root,text,color="white",fg_color="black"):
+        label = tk.Label(root,text=text,
+                         font="none 12 bold",
+                         bg=color,
+                         fg=fg_color
+                         )
+        return label
+
+    @staticmethod
+    def create_post_label(root, h1, text):
+        can = tk.Canvas(root, height=200, width=500, bg="red")
+        label = tk.Label(can, text=h1, font="none 20 bold", height=0, width=len(h1) + 1)
+        txt = tk.Label(can, text=text, font="none 12", height=0, width=len(text) + 1, bg="red")
+        txt.place_configure(x=100, y=100)
+        label.place_configure(x=10, y=10)
+        can.pack_configure(padx=250, pady=50)
+        return can
+
+    @staticmethod
+    def create_entry(root,text_var):
+        return tk.Entry(root,textvariable=text_var,)
+
+    @staticmethod
+    def create_button(root,text,func,state):
+        return tk.Button(root, text=text, command=func, state=state,highlightbackground="blue")
 
 class BasicWin:
-    def __init__(self,win:tk.Tk,geometry,app):
+    def __init__(self, win: tk.Tk, geometry, app):
         self.win = win
         self.app = app
         self.win.geometry(geometry)
 
+    def load(self): ...
 
-    def load(self):...
-
-    def kill(self):...
+    def kill(self): ...
 
     def mainloop(self):
         self.win.mainloop()
 
+
 class ExploreWin(BasicWin):
 
-    def __init__(self,win,geometry,app):
-        super(ExploreWin, self).__init__(win,geometry,app)
+    def __init__(self, win, geometry, app):
+        super(ExploreWin, self).__init__(win, geometry, app)
 
         self.load()
 
-    def update_win(self,my_canvas):
+    def update_win(self, my_canvas):
         self.win.update()
         my_canvas.configure(scrollregion=my_canvas.bbox("all"))
 
@@ -50,68 +87,101 @@ class ExploreWin(BasicWin):
 
         my_canvas.create_window((0, 0), window=second_frame, anchor="nw")
         btn = tk.Button(second_frame, text="create",
-                        command=lambda: self.onclick(my_canvas,second_frame, "my post", "im love eat bananas in the sea"))
+                        command=lambda: self.onclick(my_canvas, second_frame, "my post",
+                                                     "im love eat bananas in the sea"))
         btn.pack()
-        for i in range(15):
-            self.create_post_label(second_frame,f"mypost{i}","im like you").pack()
-        self.update_win(my_canvas)
-
-    def create_post_label(self,root, h1, text):
-        can = tk.Canvas(root,height=200,width=500,bg="red")
-        label = tk.Label(can, text=h1, font="none 20 bold", height=0, width=len(h1) + 1)
-        txt = tk.Label(can, text=text, font="none 12", height=0, width=len(text) + 1, bg="red")
-        txt.place_configure(x=100, y=100)
-        label.place_configure(x=10, y=10)
-        can.pack_configure(padx=250,pady=50)
-        return can
 
 
-    def reload(self,):
 
+    def reload(self, ):
         for post in self.app.posts:
             post.pack()
 
-    def onclick(self,c,root, h1, text):
-
-        label = self.create_post_label(root, h1, text)
+    def onclick(self, c, root, h1, text):
+        label = ComponentCreator.create_post_label(root, h1, text)
         self.app.posts.append(label)
         self.update_win(c)
         self.reload()
 
-class LoginWin(BasicWin):
-    def __init__(self,win,geometry,app):
-        super(LoginWin, self).__init__(win,geometry,app)
-        self.login_btn = tk.Button(self.win,text="click",command=self.login)
 
-    def login(self):
-        self.kill()
-        self.app.state = "explore"
+class LoginWin(BasicWin):
+    def __init__(self, win, geometry, app):
+        super(LoginWin, self).__init__(win, geometry, app)
+
+        self.login_btn = ComponentCreator.create_button(self.win, "Login", self.login_btn_onclick,"disabled")
+        self.email_var = tk.StringVar()
+        self.password_var = tk.StringVar()
+        self.email_field = ComponentCreator.create_entry(self.win,self.email_var)
+        self.password_field = ComponentCreator.create_entry(self.win,self.password_var)
+        self.validate_job = None
+        self.email_text = ComponentCreator.create_text_label(self.win,"Your Email:")
+        self.pass_text = ComponentCreator.create_text_label(self.win,"Your Password:")
+
+    def login_btn_onclick(self):
+
+
+        response = self.app.login(self.email_var.get(), self.password_var.get())
+        if response == 1:
+            self.win.after_cancel(self.validate_job)
+            self.kill()
+            self.app.state = AppStates.EXPLORE
+            self.app.update_content()
+
+        else:
+            # show error
+            self.password_field.delete(0, tk.END)
+
     def kill(self):
+        self.email_text.destroy()
+        self.pass_text.destroy()
         self.login_btn.destroy()
+        self.email_field.destroy()
+        self.password_field.destroy()
         self.win.clipboard_clear()
 
-    def load(self):
+    def validate_input(self):
+        if len(self.email_var.get()) < 3:
+            self.login_btn.config(state="disabled")
+        else:
+            self.login_btn.config(state="active")
+        self.validate_job = self.win.after(1, self.validate_input)
 
-        self.login_btn.pack()
+    def load(self):
+        pad_y = 10
+        self.email_text.pack(pady=pad_y)
+        self.email_field.pack(pady=pad_y)
+        self.pass_text.pack(pady=pad_y)
+        self.password_field.pack(pady=pad_y)
+        self.login_btn.pack(pady=pad_y)
+        self.validate_input()
 
 
 
 class App:
     def __init__(self):
-        self.state = "login"
+        self.state = AppStates.LOGIN
         self.win = tk.Tk()
         self.root = LoginWin(self.win, "800x800", self)
         self.root.load()
         self.posts = []
-        threading.Thread(target=self.state_gui,daemon=True).start()
+
+    def login(self, email, password):
+        hash = hashlib.md5(password.encode())
+        # requests login
+        # response 200 -> return ok
+        # response bad return not ok
+        print(email, hash.hexdigest())
+        return 1
+
+    def update_content(self):
+        threading.Thread(target=self.state_gui, daemon=True).start()
 
     def state_gui(self):
-        while True:
-            time.sleep(0.2)
-            print(self.state)
-            if self.state == "explore" and type(self.root) != ExploreWin:
-                self.root.kill()
-                self.root = ExploreWin(self.win,"1000x1000",self)
+        print(self.state)
+        if self.state == AppStates.EXPLORE and type(self.root) != ExploreWin:
+            self.root.kill()
+            self.root = ExploreWin(self.win, "1000x1000", self)
+
 
 def run_app():
     app = App()
